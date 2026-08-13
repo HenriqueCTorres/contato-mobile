ae1e795e5d2a65bc81b57e7d6084068e4d79ccac
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-13 10:25:45 -0300

    feat: adição de autenticação

--- index.ts
+++ index.ts
@@ -1,35 +1,32 @@
 import express from 'express';
-import type {Express, Request, Response} from 'express';
+import type {Express, Request, Response, NextFunction} from 'express';
+import contatoController from './controller/ContatoController.mjs';
+import { Usuario } from './model/Usuario.mjs';
+import jwt from 'jsonwebtoken';
+import { authMiddleware, signIn } from './security/Authentication.mjs';
 
 const app : Express = express();
 
 app.use(express.json());
 
-const lista : Array<object> = [
-    {"nome": "Joao Silva", "telefone": "(11) 1111-1111", "email": "joao@teste.com"},
-    {"nome": "Maria Silva", "telefone": "(11) 2222-2222", "email": "maria@teste.com"},
-    {"nome": "Jose Silva", "telefone": "(11) 3333-3333", "email": "jose@teste.com"},
-];
-
 // const app = express();
+
+app.post("/signin", signIn);
 
 console.log("Servidor Backend da Gestão de Contatos");
 
 app.get("/", (req : Request, res : Response) => {
     console.log("Executado GET no recurso /");
-    res.status(200).send("Bem vindo ao servidr de contatos");
+    res.status(200).send("Bem vindo ao servidor de contatos");
 });
 
-app.get("/contato", (req : Request, res : Response) => {
-    console.log("Executado GET no recurso /");
-    res.status(200).send( lista );
-});
+// app.get("/contato", authMiddleware, contatoController.pesquisarTodos );
+// app.post("/contato", authMiddleware, contatoController.criar );
 
-app.post("/contato", (req: Request, res : Response) => {
-    const contato = req.body;
-    lista.push( contato );
-    res.status(201).send("Contato criado com sucesso");
-});
+app.use(authMiddleware);
+app.get("/contato", contatoController.pesquisarTodos );
+app.post("/contato", contatoController.criar );
+
 
 app.listen( 3000, () => {
     console.log("Servidor ativo, e ouvindo conexões");
